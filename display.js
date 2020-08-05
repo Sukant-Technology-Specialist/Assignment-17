68f721433e3cd2729ecb66a1fc0b4d558560356d
Sukant-Technology-Specialist <66523686+Sukant-Technology-Specialist@users.noreply.github.com>
2020-08-05 23:43:57 +0530

    Add files via upload
    Initial Version

---
+++ display.js
@@ -0,0 +1,52 @@
+loadingParents = new Promise((resolve,reject)=>{
+    let parentElements = `<main>
+    <section>
+        <h1>Javascript Assignment - Country Details</h1>
+        <div class="container-fluid" id="country-details">
+        </div>
+    </section>
+</main>`
+    document.body.innerHTML = parentElements;
+    resolve();
+})
+loadCountries = new Promise((resolve,reject)=>{
+    let url = 'https://restcountries.eu/rest/v2/all';
+    let countryDetails = document.getElementById('country-details');
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {getRestCountries(data);resolve();})
+        .catch(err => {console.log("Error:", err);reject();});
+    function getRestCountries(param) {
+        let tags = '';
+        let count = 1;
+        let divRow = document.createElement('div');
+        for (let i = 0; i < param.length; i++) {
+            divRow.setAttribute('class', 'row');
+            let divCol = document.createElement('div');
+            divCol.setAttribute('class', 'col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 mt-3');
+            tags = `
+                <div class="card card-custom-css">
+                    <div class="card-header">
+                       ${count}). ${param[i].name}
+                    </div>
+                    <img src="${param[i].flag}" id="${param[i].alpha2Code}" class="card-img-top cust-card-img" alt="${param[i].name}">
+                    <div class="card-body card-body-custom-css">
+                        <p class="card-text">
+                            Capital: <span class="badge badge-success"> ${param[i].capital} </span><br>
+                            Country Codes: <span class="badge "> ${param[i].alpha2Code}, ${param[i].alpha3Code}</span><br>
+                            Region: <span class="badge "> ${param[i].region}</span>
+                        </p>
+                    </div>
+                </div>
+                `;
+            count++;
+            divCol.innerHTML = tags;
+            divRow.appendChild(divCol);
+            countryDetails.appendChild(divRow);
+        }
+    }
+})
+
+loadingParents
+.then(loadCountries)
+.catch(error=>console.log('error',error));
